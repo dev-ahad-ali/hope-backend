9b95deebab8e7ba5389f0e3f68b63ea2ac7d5019
dev-ahad-ali <ahadali.dev@gmail.com>
2024-07-27 00:26:25 +0600

    config middleware added on app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' })); // use 'extended' for sending nested object through url
+app.use(express.static('public'));
+app.use(cookieParser());
 
 export { app };
