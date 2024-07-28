786698225f6413fd470f671e33208e7d62a11fb0
dev-ahad-ali <ahadali.dev@gmail.com>
2024-07-29 01:27:42 +0600

    Update: multer middleware and required validation added on user register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,23 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
+import { ApiError } from '../utils/ApiError.js';
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: 'ok',
-  });
+  // get user information from frontend ✅
+  // validation - not empty ✅
+  // check if user already exist : email, username
+  // check for image : avatar check
+  // upload them to cloudinary : avatar check
+  // create user object - create entry in Db
+  // remove password and refresh token filed from response
+  // check for user creation
+  // return response
+
+  const { userName, email, password } = req.body;
+  console.log('email:', email);
+
+  if ([userName, email, password].some((field) => field?.trim() === '')) {
+    throw new ApiError(400, 'All fields are required');
+  }
 });
 
 export { registerUser };
