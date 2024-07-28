fd0e59eb6c038ec0709dfad212d3635b4572ab3c
dev-ahad-ali <ahadali.dev@gmail.com>
2024-07-29 01:48:54 +0600

    Update: image file check method added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,8 +5,8 @@ import { User } from '../models/user.model.js';
 const registerUser = asyncHandler(async (req, res) => {
   // get user information from frontend ✅
   // validation - not empty ✅
-  // check if user already exist : email, username
-  // check for image : avatar check
+  // check if user already exist : email, username ✅
+  // check for image : avatar check ✅
   // upload them to cloudinary : avatar check
   // create user object - create entry in Db
   // remove password and refresh token filed from response
@@ -25,6 +25,11 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (userAlreadyExist) throw new ApiError(409, 'User with email or username already exist');
+
+  const avatarLocalPath = req.files?.avatar[0]?.path; // console log the "req.files" to see all the info
+  const coverLocalPath = req.files?.cover[0]?.path;
+
+  if (!avatarLocalPath) throw new ApiError(400, 'Avatar file id required');
 });
 
 export { registerUser };
