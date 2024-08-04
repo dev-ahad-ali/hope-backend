09648eb07f2601a9fbd13753a999889c90305ac7
dev-ahad-ali <ahadali.dev@gmail.com>
2024-08-04 15:34:15 +0600

    Update: generate access token and refresh token method created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import { User } from '../models/user.model.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, 'Something went wrong while generating access and refresh token');
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   // get user information from frontend ✅
   // validation - not empty ✅
@@ -84,6 +99,8 @@ const loginUser = asyncHandler(async (req, res) => {
   const isPasswordValid = await user.isPasswordCorrect(password);
 
   if (!isPasswordValid) throw new ApiError(401, 'Invalid user credentials');
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id);
 });
 
 export { registerUser, loginUser };
