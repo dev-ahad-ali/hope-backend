ff43d49b8815fe2b9154501d89062bc5cd678894
dev-ahad-ali <ahadali.dev@gmail.com>
2024-08-08 23:25:32 +0600

    Fix: Cookie option added on clear cookie when loging out

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -146,8 +146,8 @@ const logoutUser = asyncHandler(async (req, res) => {
 
   return res
     .status(200)
-    .clearCookie('accessToken')
-    .clearCookie('refreshToken')
+    .clearCookie('accessToken', options)
+    .clearCookie('refreshToken', options)
     .json(new ApiResponse(200, {}, 'User logged out successfully'));
 });
 
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,7 +3,8 @@ import { asyncHandler } from '../utils/asyncHandler.js';
 import jwt from 'jsonwebtoken';
 import { User } from '../models/user.model.js';
 
-export const verifyJWT = asyncHandler(async (req, res, next) => {
+// you can right "_" when you are not using any of the parameters "req,res,next"
+export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token = req.cookies?.accessToken || req.header('Authorization')?.replace('Bearer ', '');
 
