91f833160f13bbc29e7771b02b36191efd453b88
dev-ahad-ali <ahadali.dev@gmail.com>
2024-08-08 20:33:00 +0600

    Update: log in and log out route added

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,18 +4,22 @@ import jwt from 'jsonwebtoken';
 import { User } from '../models/user.model.js';
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
-  const token = req.cookies?.accessToken || req.header('Authorization')?.replace('Bearer ', '');
+  try {
+    const token = req.cookies?.accessToken || req.header('Authorization')?.replace('Bearer ', '');
 
-  if (!token) throw new ApiError(401, 'Unauthorized access');
+    if (!token) throw new ApiError(401, 'Unauthorized access');
 
-  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-  const user = await User.findById(decodedToken._id).select('-password -refreshToken');
+    const user = await User.findById(decodedToken._id).select('-password -refreshToken');
 
-  // Todo: discuss about frontend
-  if (!user) throw new ApiError(401, 'Invalid Access Token');
+    // Todo: discuss about frontend
+    if (!user) throw new ApiError(401, 'Invalid Access Token');
 
-  req.user = user;
+    req.user = user;
 
-  next();
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || 'Invalid Access Token');
+  }
 });
